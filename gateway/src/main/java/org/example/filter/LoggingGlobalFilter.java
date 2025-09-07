992ce8ef9f7813b1ccf7afb01d2bb100000c2c44
leang <leangk49@gmail.com>
2025-09-07 16:56:40 +0700

    Test web mvc gateway

--- gateway/src/main/java/org/example/filter/LoggingGlobalFilter.java
+++ gateway/src/main/java/org/example/filter/LoggingGlobalFilter.java
@@ -1,35 +1,35 @@
-package org.example.filter;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-@Slf4j
-@Component
-public class LoggingGlobalFilter implements GlobalFilter, Ordered {
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("Request URL: " + exchange.getRequest().getURI());
-        System.out.println("Request Method: " + exchange.getRequest().getMethod());
-        System.out.println("Request headers:");
-        exchange.getRequest().getHeaders()
-                .forEach((k, v) -> System.out.println(k + " = " + v));
-
-        return chain.filter(exchange).doOnSuccess(aVoid -> {
-            // Log response status
-            System.out.println("Response Status Code: " + exchange.getResponse().getStatusCode());
-            exchange.getResponse().getHeaders()
-                    .forEach((k, v) -> System.out.println(k + " : " + v));
-        });
-    }
-
-    @Override
-    public int getOrder() {
-        return -1;
-    }
-}
+//package org.example.filter;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.Ordered;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//@Slf4j
+//@Component
+//public class LoggingGlobalFilter implements GlobalFilter, Ordered {
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        System.out.println("Request URL: " + exchange.getRequest().getURI());
+//        System.out.println("Request Method: " + exchange.getRequest().getMethod());
+//        System.out.println("Request headers:");
+//        exchange.getRequest().getHeaders()
+//                .forEach((k, v) -> System.out.println(k + " = " + v));
+//
+//        return chain.filter(exchange).doOnSuccess(aVoid -> {
+//            // Log response status
+//            System.out.println("Response Status Code: " + exchange.getResponse().getStatusCode());
+//            exchange.getResponse().getHeaders()
+//                    .forEach((k, v) -> System.out.println(k + " : " + v));
+//        });
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return -1;
+//    }
+//}
--- gateway/src/main/java/org/example/filter/MyGlobalFilter.java
+++ gateway/src/main/java/org/example/filter/MyGlobalFilter.java
@@ -1,32 +1,32 @@
-package org.example.filter;
-
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-@Component
-public class MyGlobalFilter implements GlobalFilter, Ordered {
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-      return chain.filter(
-              exchange.mutate().request(
-                   builder -> builder.header("testing_header","header_value")
-              ).build()
-      ).doOnEach(voidSignal -> {
-          exchange.getResponse().beforeCommit(() -> {
-              exchange.getResponse().getHeaders().add("key","new value");
-              return Mono.empty();
-          });
-      });
-    }
-
-    @Override
-    public int getOrder() {
-        return -100;
-    }
-}
-
+//package org.example.filter;
+//
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.Ordered;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//@Component
+//public class MyGlobalFilter implements GlobalFilter, Ordered {
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//      return chain.filter(
+//              exchange.mutate().request(
+//                   builder -> builder.header("testing_header","header_value")
+//              ).build()
+//      ).doOnEach(voidSignal -> {
+//          exchange.getResponse().beforeCommit(() -> {
+//              exchange.getResponse().getHeaders().add("key","new value");
+//              return Mono.empty();
+//          });
+//      });
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return -100;
+//    }
+//}
+//
